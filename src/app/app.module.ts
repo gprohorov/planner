b2f0599f74d512f2b2dc49b2ac46c79b0c2b7969
george <yuramail222@gmail.com>
2020-01-23 17:40:00 +0200

    table sort

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,7 @@ import { AppComponent } from './app.component';
 import { CategoriesComponent } from './view/categories/categories.component';
 import {DataHandlerService} from './service/data-handler.service';
 import { TasksComponent } from './view/tasks/tasks.component';
-import {MatSortModule, MatTableModule} from '@angular/material';
-import {MatPaginatorModule} from '@angular/material/typings/esm5/paginator';
+import {MatPaginatorModule, MatSortModule, MatTableModule} from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 
