35e59f3131e3e9849770a95d2b913fce298fc1c9
george <yuramail222@gmail.com>
2020-01-27 17:47:19 +0200

    DAO

--- src/app/dao/interface/CategoryDAO.ts
+++ src/app/dao/interface/CategoryDAO.ts
@@ -2,10 +2,10 @@ import {Category} from '../../../../../Desktop/Todo/src/app/model/Category';
 import {CommonDAO} from './CommonDAO';
 import {Observable} from 'rxjs';
 
-// специфичные методы для работы с категориями (которые не входят в обычный CRUD)
+
 export interface CategoryDAO extends CommonDAO<Category> {
 
-    // поиск категорий по названию
+
     search(title: string): Observable<Category[]>;
 
 }
--- src/app/dao/interface/PriorityDAO.ts
+++ src/app/dao/interface/PriorityDAO.ts
@@ -1,9 +1,9 @@
 import {Priority} from '../../../../../Desktop/Todo/src/app/model/Priority';
 import {CommonDAO} from './CommonDAO';
 
-// специфичные методы для работы приоритетами (которые не входят в обычный CRUD)
+
 export interface PriorityDAO extends CommonDAO<Priority> {
 
-    // здесь будут специфичные методы для работы с категориями (на будущее)
+
 
 }
